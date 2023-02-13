ed7548776728ed457719b6d47e155aa6ca94bf71
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-02-12 22:07:42 -0600

    Tutorial 1 - Paso 6

--- Intro to lit/Tutorial 1.js
+++ Intro to lit/Tutorial 1.js
@@ -1,30 +1,40 @@
-//PASO 5
+//PASO 6
 import {LitElement, html} from 'lit';
 
-export class MoreExpressions extends LitElement {
+export class ToDoList extends LitElement {
   static properties = {
-    checked: {},
+    _listItems: {state: true},
   };
 
   constructor() {
     super();
-    this.checked = false;
+    this._listItems = [
+      {text: 'Start Lit tutorial', completed: true},
+      {text: 'Make to-do list', completed: false},
+    ];
   }
 
   render() {
     return html`
-      <div>
-        <input type="text" ?disabled=${!this.checked} value="Hello there.">
-      </div>
-      <label><input type="checkbox" @change=${
-        this.setChecked
-      }> Enable editing</label>
+      <h2>To Do</h2>
+      <ul>
+        ${this._listItems.map((item) => html`<li>${item.text}</li>`)}
+      </ul>
+      <input id="newitem" aria-label="New item">
+      <button @click=${this.addToDo}>Add</button>
     `;
   }
 
-  setChecked(event) {
-    this.checked = event.target.checked;
+  get input() {
+    return this.renderRoot?.querySelector('#newitem') ?? null;
+  }
+
+  addToDo() {
+    this._listItems = [
+      ...this._listItems,
+      {text: this.input.value, completed: false},
+    ];
+    this.input.value = '';
   }
 }
-customElements.define('more-expressions', MoreExpressions);
-
+customElements.define('todo-list', ToDoList);
