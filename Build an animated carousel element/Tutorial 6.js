de9ba6c29d64a85a94c98bd49e3d0682d8046249
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-02-13 21:42:20 -0600

    Tutorial 6 - Paso 2

--- Build an animated carousel element/Tutorial 6.js
+++ Build an animated carousel element/Tutorial 6.js
@@ -1,2 +1,18 @@
-//PASO 1
-//...
+//PASO 2
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {styles} from './styles.js';
+
+@customElement('motion-carousel')
+export class MotionCarousel extends LitElement {
+  static styles = styles;
+
+  render() {
+    return html`
+      <div class="fit">
+        <slot></slot>
+      </div>
+    `;
+  }
+
+}
