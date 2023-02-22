ab0c4f35512d13b2c5c42ed458c2d02a7c216458
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-02-22 10:06:53 -0600

    Turorial 7 - Paso 4

--- Build a word viewer/Tutorial 7.js
+++ Build a word viewer/Tutorial 7.js
@@ -1,18 +1,22 @@
-//PASO 3
+//PASO 4
 import {html, LitElement} from 'lit';
 
 class WordViewer extends LitElement {
   static properties = {
+    idx: {state: true},
     words: {},
   };
 
   constructor() {
     super();
+    this.idx = 0;
     this.words = 'initial value';
   }
 
   render() {
-    return html`<pre>${this.words}</pre>`;
+    const splitWords = this.words.split('.');
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
   }
 }
 customElements.define('word-viewer', WordViewer);
